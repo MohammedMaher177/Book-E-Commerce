f06c20550354f772c2e47d5d25ade9afe5afe3b8
MohammedMaher177 <anahammo777@gmail.com>
2023-09-07 20:08:51 +0300

    test

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
+dotenv.config();
 
-dotenv.config()
+import express from "express";
+import { bootstrap } from "./src/app.routes.js";
+import cookie_parser from "cookie-parser";
 
-import express from 'express'
-import { bootstrap } from './src/app.routes.js'
-import cookie_parser from "cookie-parser"
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.use(cookie_parser("1234"));
+app.use(express.json());
 
-app.use(cookie_parser("1234"))
-app.use(express.json())
+bootstrap(app);
 
-bootstrap(app)
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/app.routes.js
+++ src/app.routes.js
@@ -1,29 +1,21 @@
-import connectionDb from "../DB/dbConnection.js"
-import authRouter from "./modules/auth/auth.routes.js"
-import userRouter from "./modules/user/user.routes.js"
-import { AppError } from "./util/ErrorHandler/AppError.js"
-
-
+import connectionDb from "../DB/dbConnection.js";
+import authRouter from "./modules/auth/auth.routes.js";
+import userRouter from "./modules/user/user.routes.js";
+import { AppError } from "./util/ErrorHandler/AppError.js";
 
 export const bootstrap = (app) => {
-    connectionDb()
+  connectionDb();
 
-    // app.get("/", (req, res, next) => res.json("hello orld"))
-    app.use("/api/v1/auth", authRouter)
-    app.use("/api/v1/users", userRouter)
+  app.use("/api/v1/auth", authRouter);
+  app.use("/api/v1/users", userRouter);
 
+  app.all("*", (req, res, next) => {
+    next(new AppError("Page Not Found", 404));
+  });
 
-
-
-
-
-    app.all("*", (req, res, next) => {
-        next(new AppError("Page Not Found", 404))
-    })
-
-    app.use((err, req, res, next) => {
-        const error = err.message
-        const code = err.statusCode || 500
-        res.status(code).json({ message: "Error", error, stack: err.stack })
-    })
-}
+  app.use((err, req, res, next) => {
+    const error = err.message;
+    const code = err.statusCode || 500;
+    res.status(code).json({ message: "Error", error, stack: err.stack });
+  });
+};

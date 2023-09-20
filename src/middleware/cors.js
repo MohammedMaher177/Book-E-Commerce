618cd5d9c1fba475135f5a78d1d8405a13112c21
Mohamed Elsayed <96896650+MohamedElsayed2202@users.noreply.github.com>
2023-09-20 15:44:19 +0300

    fix

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -7,10 +7,10 @@ const cors = (req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTION');
     res.setHeader('Access-Control-Allow-Credentials', 'true')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-    res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie, Set-Cookie2');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization,');
+    // res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie, Set-Cookie2');
 
     next();
 }

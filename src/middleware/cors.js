df9e68528c71beaa8d9d9b698f13bb6c1ea28f48
Mohamed Elsayed <96896650+MohamedElsayed2202@users.noreply.github.com>
2023-09-20 15:22:10 +0300

    fix

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -10,6 +10,7 @@ const cors = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie, Set-Cookie2');
 
     next();
 }

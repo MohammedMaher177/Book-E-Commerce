779adb768f623e30ef3bd0891e2e7e302330d730
MohammedMaher177 <anahammo777@gmail.com>
2023-10-22 12:02:35 +0300

    cart

--- src/modules/cart/controller/cart.controller.js
+++ src/modules/cart/controller/cart.controller.js
@@ -4,12 +4,25 @@ import { AppError } from "../../../util/ErrorHandler/AppError.js";
 import { catchError } from "../../../util/ErrorHandler/catchError.js";
 
 function calcPrice(cart) {
-  let totalPrice = 0;
-  cart.products.forEach((ele) => {
-    totalPrice += ele.qty * ele.price;
-  });
-  cart.totalAmount = totalPrice;
+  return cart.totalAmount = cart.books.reduce(
+    (partialSum, book) => partialSum + book.totalPrice, 0
+  );
 }
+
+function calcDiscount (cart){
+  if (cart.discount)
+  return cart.totalAmountAfterDisc =
+    cart.totalAmount - cart.totalAmount * cart.discount;
+  else return cart.totalAmountAfterDisc = cart.totalAmount
+}
+
+async function existingBook (id){
+  const existBook = await bookModel
+  .findById(id)
+  .select("price");
+  return existBook
+}
+
 
 export const getCartDetails = catchError(async (req, res, next) => {
   const { _id } = req.user;
@@ -23,69 +36,65 @@ export const getCartDetails = catchError(async (req, res, next) => {
 export const addToCart = catchError(async (req, res, next) => {
   const { _id } = req.user;
 
-  const existBook = await bookModel
-    .findById(req.body.book)
-    .select("price");
+  const existBook = await existingBook(req.body.book)
+  
   if (!existBook) throw new AppError("In-Valid book ID", 404);
-    console.log(existBook);
   if (req.body.qty < 1) throw new AppError("U Can not add 0 Qty", 403);
+
   req.body.qty = req.body.qty || 1;
   req.body.totalPrice = req.body.qty * existBook.price;
   req.body.price = existBook.price;
   let cart = await cartModel.findOne({ user: _id });
-  if (!cart) {
+
+  if (!cart) 
+  {
     cart = await cartModel.create({
       user: _id,
       books: req.body,
-      totalAmount: req.body.price,
+      totalAmount: req.body.totalPrice,
     });
     return res.status(201).json({ message: "success", cart });
   }
+  
   let index = cart.books.findIndex(
     (el) => el.book._id == req.body.book
   );
   if (index === -1) {
     cart.books.push(req.body);
   } else {
-    req.body.qty += cart.books[index].qty;
+    req.body.qty = cart.books[index].qty;
+    req.body.totalPrice = req.body.qty * cart.books[index].price;
     cart.books[index] = req.body;
   }
-  cart.totalAmount = cart.books.reduce(
-    (partialSum, a) => partialSum + a.price,
-    0
-  );
+  calcPrice(cart)
 
-  if (cart.discount)
-    cart.totalAmountAfterDisc =
-      cart.totalAmount - cart.totalAmount * cart.discount;
-
+  calcDiscount(cart)
   await cart.save();
   return res.status(201).json({ message: "success", cart });
 });
 
 export const updateCartQty = catchError(async (req, res, next) => {
   const { _id } = req.user;
-  const { product } = req.body;
+  const { book } = req.body;
   let cart = await cartModel.findOne({ user: _id });
+
   if (!cart) return new AppError("Cart Not Found", 404);
-  if (!req.body.qty) throw new AppError("In-Valid Product QTY", 403);
-  let index = cart.products.findIndex((el) => el.product.id == product);
-  if (index == -1) throw new AppError("In-Valid Product ID", 404);
-  req.body.price = req.body.qty * cart.products[index].product.price;
-  cart.products[index] = req.body;
-  cart.totalAmount = cart.products.reduce(
-    (partialSum, a) => partialSum + a.price,
-    0
-  );
+  if (!req.body.qty) throw new AppError("In-Valid book QTY", 403);
 
-  if (cart.discount)
-    cart.totalAmountAfterDisc =
-      cart.totalAmount - cart.totalAmount * cart.discount;
+  let index = cart.books.findIndex((el) => el.book.id == book);
+  if (index == -1) throw new AppError("In-Valid book ID", 404);
 
+  req.body.totalPrice = req.body.qty * cart.books[index].book.price;
+  req.body.price = cart.books[index].book.price
+  cart.books[index] = req.body;
+
+  calcPrice(cart);
+
+  calcDiscount(cart)
   await cart.save();
   return res
-    .status(201)
-    .json({ message: "success", cart, TotalCartPrice: cart.totalAmount });
+    .status(202)
+    .json({ message: "success", cart});
 });
 
 export const removeItem = catchError(async (req, res, next) => {
@@ -93,16 +102,12 @@ export const removeItem = catchError(async (req, res, next) => {
   const { id } = req.params;
   let cart = await cartModel.findOne({ user: _id });
   if (!cart) return new AppError("Cart Not Found", 404);
-  let index = cart.products.findIndex((el) => el.product.id == id);
+  let index = cart.books.findIndex((el) => el.book.id == id);
   if (index == -1) throw new AppError("In-Valid Product ID", 404);
-  cart.products.splice(index, 1);
-  cart.totalAmount = cart.products.reduce(
-    (partialSum, a) => partialSum + a.price,
-    0
-  );
-  if (cart.discount)
-    cart.totalAmountAfterDisc =
-      cart.totalAmount - cart.totalAmount * cart.discount;
+  cart.books.splice(index, 1);
+  calcPrice(cart)
+
+  calcDiscount(cart)
   await cart.save();
   return res.json({ message: "success", cart });
 });
